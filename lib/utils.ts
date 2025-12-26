73a24ea0a51494c69ba2c163cbb95473af79f2c9
saurabh chauhan <sourabh1202@gmail.com>
2025-12-26 16:18:10 +0200

    added utl

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { randomUUID } from 'crypto';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const ANON_COOKIE = 'anon_id';
+
+export function generateAnonId() {
+  return randomUUID();
+}
+
+
