d825958fe8245447ef2c2ceb49a43f110be34614
saurabh chauhan <sourabh1202@gmail.com>
2026-04-01 20:08:01 +0300

    added few allowed images domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
+  images:{
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "fastly.picsum.photos",
+      }
+    ]
+  }
   /* config options here */
 };
 
+
 export default nextConfig;
+
+
